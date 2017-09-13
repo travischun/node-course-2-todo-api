c351906b36379e67fc39a5cda48396792c8b1fdb
Travis Chun <tchun@us.ibm.com>
2017-09-13 17:23:39 -0500

    Updated POST /user

--- server/server.js
+++ server/server.js
@@ -106,13 +106,16 @@ app.patch('/todos/:id',(req,res)=>{
 		res.status(400).send();
 	});
 });
-
+//POST /users
 app.post('/users',(req,res)=>{
 	var body = _.pick(req.body,['email','password']);
 	var user = new User(body);
-	user.save().then((doc)=>{
-		res.send(doc);
-	},(e)=>{
+
+	user.save().then(()=>{
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((e)=>{
 		res.status(400).send(e);
 	});
 });
